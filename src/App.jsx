72ca4ab6e7a16c76f63058b9a5283ee00866e04d
KyrylMikheiev <kyrylmikheiev20.11@gmail.com>
2024-03-10 18:08:55 +0100

    ready game

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,10 @@ export default function App() {
     const [isPressed, setIsPressed] = useState(true);
     const [gameOutcome, setGameOutcome] = useState("I will tell the outcome of each game!");
     const [tiles, setTiles] = useState(Array(9).fill(null));
-    const [playerTurn, setPlayerTurn] = useState("X");
-    const [startSign, setStartSign] = useState(true);
+    const [move, setMove] = useState("X");
+    const [playerMove, setPlayerMove] = useState("X");
     const [isGameRunning, setIsGameRunning] = useState(true);
+
     const [wins, setWins] = useState(0);
     const [draws, setDraws] = useState(0);
     const [looses, setLooses] = useState(0);
@@ -18,50 +19,53 @@ export default function App() {
 
     function handleTileClick(index) {
       if (tiles[index]) return;
-      setPlayerTurn(playerTurn === "X" ? "O" : "X");
+      setMove(move === "X" ? "O" : "X");
       const newTiles = [...tiles];
-      newTiles[index] = playerTurn;
+      newTiles[index] = move;
       setTiles(newTiles);
+    }
+
+    function endGame(sign) {
+      if (sign === playerMove) {
+        setGameOutcome("You won!");
+        setWins(wins + 1);
+      } else {
+        setGameOutcome("You lost!");
+        setLooses(looses + 1);
+      }
+      setIsGameRunning(false);
     }
 
     const handleButtonClick = () => {
       if (tiles.every((tile) => tile === null)) {
         return;
       }
-      setStartSign(!startSign);
-      setPlayerTurn(startSign ? "X" : "O");
+      setPlayerMove(playerMove === "X" ? "O" : "X");
+      setGameOutcome("Game is running ...")
+      setMove("X");
       setIsGameRunning(true);
       setTiles(Array(9).fill(null));
-        setGameOutcome("Game is running...");
-        setIsPressed(true);
+      setIsPressed(true);
 
     };
 
     useEffect(() => {
       if (tiles[0] === tiles[1] && tiles[1] === tiles[2] && tiles[0] !== null) {
-        setGameOutcome(`Player ${tiles[0]} wins!`);
-        setIsGameRunning(false);
+        endGame(tiles[0]);
       } else if (tiles[3] === tiles[4] && tiles[4] === tiles[5] && tiles[3] !== null) {
-        setGameOutcome(`Player ${tiles[3]} wins!`);
-        setIsGameRunning(false);
+        endGame(tiles[3]);
       } else if (tiles[6] === tiles[7] && tiles[7] === tiles[8] && tiles[6] !== null) {
-        setGameOutcome(`Player ${tiles[6]} wins!`);
-        setIsGameRunning(false);
+        endGame(tiles[6]);
       } else if (tiles[0] === tiles[3] && tiles[3] === tiles[6] && tiles[0] !== null) {
-        setGameOutcome(`Player ${tiles[0]} wins!`);
-        setIsGameRunning(false);
+        endGame(tiles[0]);
       } else if (tiles[1] === tiles[4] && tiles[4] === tiles[7] && tiles[1] !== null) {
-        setGameOutcome(`Player ${tiles[1]} wins!`);
-        setIsGameRunning(false);
+        endGame(tiles[1]);
       } else if (tiles[2] === tiles[5] && tiles[5] === tiles[8] && tiles[2] !== null) {
-        setGameOutcome(`Player ${tiles[2]} wins!`);
-        setIsGameRunning(false);
+        endGame(tiles[2]);
       } else if (tiles[0] === tiles[4] && tiles[4] === tiles[8] && tiles[0] !== null) {
-        setGameOutcome(`Player ${tiles[0]} wins!`);
-        setIsGameRunning(false);
+        endGame(tiles[0]);
       } else if (tiles[2] === tiles[4] && tiles[4] === tiles[6] && tiles[2] !== null) {
-        setGameOutcome(`Player ${tiles[2]} wins!`);
-        setIsGameRunning(false);
+        endGame(tiles[2]);
       } else if (tiles.every((tile) => tile !== null)) {
         setGameOutcome("It's a draw!");
         setIsGameRunning(false);
@@ -85,7 +89,8 @@ export default function App() {
                 <h1 className={`header ${isPressed ? 'bounceText' : ''}`}>TicTacToe</h1>
                 <Stats wins={wins} draws={draws} looses={looses}/>
                 <PlayField tiles={tiles} handleTileClick={handleTileClick} disabled={!isGameRunning}/>
-                <label>{gameOutcome}</label>
+                <label className="playerMove">You play with {playerMove}</label>
+                <label className="gameOutcome">{gameOutcome}</label>
                 <button className="playAgain" onClick={handleButtonClick}>Play Again</button>
             </div>
     )
